1a3c1addd41b2a9999b7cf57762450d79899505c
YABATANIEN, Murichaduke <mail@example.com>
2021-09-02 08:00:00 +0900

    black and isort

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
 import argparse
 import csv
 import json
-import sys
 import logging
 import logging.config
+import sys
 from textwrap import dedent
 from typing import Dict, Iterable, List
 
 import MeCab
-
 
 logger = logging.getLogger(__name__)
 
 
 # see: https://unidic.ninjal.ac.jp/faq
 FEATURES_UDIC = [
-    'pos1',
-    'pos2',
-    'pos3',
-    'pos4',
-    'cType',
-    'cForm',
-    'lForm',
-    'lemma',
-    'orth',
-    'pron',
-    'orthBase',
-    'pronBase',
-    'goshu',
-    'iType',
-    'iForm',
-    'fType',
-    'fForm',
-    'kana',
-    'kanaBase',
-    'form',
-    'formBase',
-    'iConType',
-    'fConType',
-    'aType',
-    'aConType',
-    'aModType',
+    "pos1",
+    "pos2",
+    "pos3",
+    "pos4",
+    "cType",
+    "cForm",
+    "lForm",
+    "lemma",
+    "orth",
+    "pron",
+    "orthBase",
+    "pronBase",
+    "goshu",
+    "iType",
+    "iForm",
+    "fType",
+    "fForm",
+    "kana",
+    "kanaBase",
+    "form",
+    "formBase",
+    "iConType",
+    "fConType",
+    "aType",
+    "aConType",
+    "aModType",
 ]
 
 # see: https://taku910.github.io/mecab/learn.html
 FEATURES_IPADIC = [
-    'pos',  # 品詞 (e.g. 動詞)
-    'pos1',  # 品詞細分類1 (e.g. 自立)
-    'pos2',  # 品詞細分類2
-    'pos3',  # 品詞細分類3
-    'cType',  # 活用型 (e.g. 命令ｅ)
-    'cForm',  # 活用形 (e.g. 段・カ行イ音便)
-    'orthBase',  # 基本形 (書字形基本形, e.g. こおりつく)
-    'kana',  # 読み (仮名形出現形, e.g. コオリツケ)
-    'pron',  # 発音 (発音出現系, e.g. コーリツケ)
+    "pos",  # 品詞 (e.g. 動詞)
+    "pos1",  # 品詞細分類1 (e.g. 自立)
+    "pos2",  # 品詞細分類2
+    "pos3",  # 品詞細分類3
+    "cType",  # 活用型 (e.g. 命令ｅ)
+    "cForm",  # 活用形 (e.g. 段・カ行イ音便)
+    "orthBase",  # 基本形 (書字形基本形, e.g. こおりつく)
+    "kana",  # 読み (仮名形出現形, e.g. コオリツケ)
+    "pron",  # 発音 (発音出現系, e.g. コーリツケ)
 ]
 FEATURES = FEATURES_IPADIC
 
@@ -75,43 +74,42 @@ def parse_to_node(tagger: MeCab.Tagger, text: str) -> Iterable[MeCab.Node]:
 
 def _configure_logging(log_level: str) -> None:
     """Configure the logging facility"""
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s %(levelname)-8s %(name)-15s %(message)s',
-                'datefmt': '%Y-%m-%dT%H:%M:%S',
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s %(levelname)-8s %(name)-15s %(message)s",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                },
             },
-        },
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
-                'level': log_level,
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                    "level": log_level,
+                },
             },
-        },
-        'root': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-        'loggers': {},
-    })
+            "root": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+            },
+            "loggers": {},
+        }
+    )
 
 
 def handler(event, context):
     text = event["body"]
-    logger.debug('text: %s', text)
+    logger.debug("text: %s", text)
 
-    tagger = MeCab.Tagger('-d /opt/mecab-ipadic-neologd')
+    tagger = MeCab.Tagger("-d /opt/mecab-ipadic-neologd")
     out = [
-        dict(
-            surface=node.surface,
-            **parse_csv(node.feature, fieldnames=FEATURES)
-        )
+        dict(surface=node.surface, **parse_csv(node.feature, fieldnames=FEATURES))
         for node in parse_to_node(tagger, text)
     ]
-    logger.debug('out: %s', out)
+    logger.debug("out: %s", out)
 
     response = {
         "statusCode": 200,
@@ -130,39 +128,45 @@ def main(argv: List[str]) -> int:
             """
         ),
     )
-    parser.add_argument('--log-level', default='INFO')
+    parser.add_argument("--log-level", default="INFO")
     parser.add_argument(
-        '--data',
+        "--data",
         type=str,
-        help='Input data',
+        help="Input data",
         required=True,
     )
     parser.add_argument(
-        '--output',
+        "--output",
         type=str,
-        choices=['raw', 'json'],
-        help='The formatting style for the command output',
-        default='raw',
+        choices=["raw", "json"],
+        help="The formatting style for the command output",
+        default="raw",
     )
 
     # parse arguments and then set log level
     args = parser.parse_args(argv)
     _configure_logging(log_level=args.log_level)
-    logger.debug('given option: %s', vars(args))
+    logger.debug("given option: %s", vars(args))
 
     # cal handler
     response = handler(json.loads(args.data), None)
 
     # ouput
-    if args.output == 'raw':
-        sys.stdout.write(str(response) + '\n')
+    if args.output == "raw":
+        sys.stdout.write(str(response) + "\n")
 
-    elif args.output == 'json':
+    elif args.output == "json":
         sys.stdout.write(
-            json.dumps({
-                **response,
-                'body': json.loads(response['body']),
-            }, sort_keys=True, ensure_ascii=False, indent=2) + '\n'
+            json.dumps(
+                {
+                    **response,
+                    "body": json.loads(response["body"]),
+                },
+                sort_keys=True,
+                ensure_ascii=False,
+                indent=2,
+            )
+            + "\n"
         )
 
     return 0
